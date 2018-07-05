a899baa09ac64b046134b5a9f1e99a9077eb573b
Sami Kerola <kerolasa@iki.fi>
2018-07-05 21:01:28 +0100

    improve script template & cronjob
    Signed-off-by: Sami Kerola <kerolasa@iki.fi>

--- bin/cronjob.sh
+++ bin/cronjob.sh
@@ -1,33 +1,28 @@
 #!/bin/bash
+# The trap ERR and various settings are bashisms, do not change shebang.
 #
 # One line description.
 #
 # Sami Kerola <kerolasa@iki.fi>
 
-# Default settings, do not touch.
-# The trap ERR and pipefail are bashisms, do not change shebang.
-JOB_NAME="${0##*/}"
-set -e
-trap 'echo "$JOB_NAME: exit on error in line $LINENO"; exit 1' ERR
-set -u
-set -o pipefail
-
 # Begin <do not touch>
+declare -r JOB_NAME="${0##*/}"
+set -e
+trap 'echo "$JOB_NAME: exit on error in line $LINENO" >&2; exit 1' ERR
+set -o errtrace
+set -o noclobber
+set -o nounset
+set -o pipefail
 # Report errors by email only when shell is not interactive.
-if [ -t 1 ]; then
-	# Terminal exists, user is running script manually.  Do
-	# not initiate error reporting.
-	trap 'echo "$0: exit on error"; exit 1' ERR
-else
-	OUTPUTFILE=$(mktemp "/tmp/$JOB_NAME.XXXXXXXXX")
-	exec > "$OUTPUTFILE" 2>&1
-	set -x
-	MAILTO="system-admin-maillist@example.com"
-	CRONJOBLOGDIR="/tmp/$JOB_NAME"
-	SERVER=$(hostname -s)
-	TIMESTAMP=$(date --iso=ns)
+if ! [ -t 1 ]; then
+	declare -r OUTPUTFILE=$(mktemp "/tmp/$JOB_NAME.XXXXXXXXX")
+	exec 1> "$OUTPUTFILE" 2>&1
+	set -o xtrace
+	declare -r MAILTO="system-admin-maillist@example.com"
+	declare -r CRONJOBLOGDIR="/tmp/$JOB_NAME"
+	declare -r TIMESTAMP="$(date --iso=ns)"
 	trap "cat $OUTPUTFILE |
-		mailx -s \"$SERVER: $JOB_NAME failed\" $MAILTO" ERR
+		mailx -s \"$HOSTNAME: $JOB_NAME failed\" $MAILTO" ERR
 	trap 'mv "$OUTPUTFILE" "$CRONJOBLOGDIR/$JOB_NAME.$TIMESTAMP"' EXIT
 	if [ ! -d "$CRONJOBLOGDIR" ]; then mkdir -p "$CRONJOBLOGDIR"; fi
 	find "$CRONJOBLOGDIR" -name "$JOB_NAME.*" \
--- bin/script-template.sh
+++ bin/script-template.sh
@@ -10,27 +10,27 @@
 #exec > /dev/null 2>&1
 
 # Default settings, do not touch.
-SCRIPT_INVOCATION_SHORT_NAME="${0##*/}"
-set -e		# exit on errors
-# trap ERR is bashism, do not change shebang!
-trap 'echo "$SCRIPT_INVOCATION_SHORT_NAME: exit on error in line $LINENO"; exit 1' ERR
-set -u		# disallow usage of unset variables
-set -o pipefail	# make pipe writer failure to cause exit on error
+declare -r SCRIPT_INVOCATION_SHORT_NAME="${0##*/}"
+trap 'echo "$SCRIPT_INVOCATION_SHORT_NAME: exit on error in line $LINENO" >&2; exit 1' ERR
+set -o errtrace
+set -o noclobber
+set -o nounset
+set -o pipefail
 RETVAL=0
 
 # Use this function when you want to inform user about something.
 # If you don't want to get script name in front of the message,
 # which would be a bit strange, use normal echo.
 msg() {
-	echo "$SCRIPT_INVOCATION_SHORT_NAME: $*" 2>&1
+	echo "$SCRIPT_INVOCATION_SHORT_NAME: $*"
 }
 
 usage() {
-	echo "Usage:"
+	echo 'Usage:'
 	echo " $0 [-hV] [argument ...]"
-	echo ""
-	echo " -h   display this help and exit"
-	echo " -V   output version information and exit"
+	echo ''
+	echo ' -h   display this help and exit'
+	echo ' -V   output version information and exit'
 	exit "$1"
 }
 
@@ -47,15 +47,6 @@ while getopts hV OPTIONS; do
 #			;;
 		h)
 			usage 0
-			;;
-		V)
-			# With subversion
-			#svn propset svn:keywords Revision generic.sh
-			#msg '$Revision:$'
-			# When git, use post commit hook, or
-			# simply maintain a number
-			msg '0.1'
-			exit 0
 			;;
 		*)
 			usage 1
@@ -77,7 +68,7 @@ done
 SCRIPTS_LOCK="/var/lock/$SCRIPT_INVOCATION_SHORT_NAME"
 
 lockfailed() {
-	msg "cannot get lock"
+	msg 'cannot get lock'
 	exit 1
 }
 exec 42>>"$SCRIPTS_LOCK"
